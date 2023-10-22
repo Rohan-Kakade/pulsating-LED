d730c5734da7a763949f9dac066a1b3af7f858bf
Rohan Kakade <rohan.kakade@york.ac.uk>
2023-10-22 10:30:19 +0000

    Version 2

--- main.cpp
+++ main.cpp
@@ -1,21 +1,40 @@
 /*Program to make gradually increase an LED's brightness
-Uses PWM
+Uses PWM*/
 #include "mbed.h"
+
+//Declare a DigitalIn object
+DigitalIn button(BUTTON1);
+
+//Declare a PwmOut object
+PwmOut pwm(D3);  //D3 pin to generate pwm signal
+
+//function to set brigtness level
+void led_brightness_set(int brightnesslevel)
+{
+    pwm = 0.1*brightnesslevel;
+}
 
 int main() 
 {
-    //Declare a PwmOut object
-    PwmOut pwm(D3);  //D3 pin to generate pwm signal
-
+    int presscount = 0;
+    
     while(true) //run forever
     {
-        //loop to gradually increase the brightness
-        for (int i=0;i<20;++i) //20 brightness levels
+        if(button==false)
         {
-         pwm =i*0.05; //duty cycle changes by 5% each iteration
-         thread_sleep_for(50); //50 ms delay between duty cyele change
-        }
-        // while(true) { sleep(); }
-    }
+            thread_sleep_for(100); //recheck if button is pressed after 100ms to discount invalid button presses
+            if(button == false)
+            {
+                presscount++;
+                led_brightness_set(presscount); //led brigtness scaled by number of button presses
+                if (presscount >10)
+                {
+                    presscount = 0;
+                }
+           
+            }   
 
+        }
+
+    }
 }
